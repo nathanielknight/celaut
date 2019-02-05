4ef591c19a43a4738e124823e0159abd5cc4e68a
Nathaniel Knight <nknight@cfenet.ubc.ca>
2019-02-04 16:15:31 -0800

    Keep seed clean in source control

--- do.sh
+++ do.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-RULE='{"tbl":[["Zero","Zero","Zero","Three","One","One","One","Three","Zero"],["Zero","Two","Zero","One","Three","Two","Three","Two","Two"],["Two","Two","Three","Two","Two","Zero","Two","One","One"],["Three","Three","Zero","Two","Two","Three","Two","One","Two"],["One","One","One","One","Zero","Two","Three","Zero","One"],["One","Three","Zero","Three","One","Zero","Two","One","Three"],["Zero","Zero","One","Two","Three","Two","Three","Zero","Two"],["Zero","One","Two","Three","Two","Zero","One","One","Three"],["Three","Two","Three","Zero","Three","Three","Three","Zero","Zero"]]}';
+RULE='';
 rm celaut.png
 cargo run --bin generate -- $RULE
 convert celaut.png -scale 500 celaut.png
